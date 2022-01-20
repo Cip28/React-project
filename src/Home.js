cc17bca16bf88b2a68acb72730b721064fe8e598
Cip28 <ciprianbarbu38@yahoo.com>
2022-01-20 11:17:00 +0200

    Home, Header & Nav rebuilding...

--- src/Home.js
+++ src/Home.js
@@ -3,32 +3,25 @@ import {Link} from 'react-router-dom'
 
 
 export class Home extends Component {
-
     render() {
         return (
-            <div>
-                <section id="hero" className="d-flex align-items-center">
+            <section id="hero" className="d-flex align-items-center">
                  <div className="containerz">
-                <div className="row">
-                    <div className="col-lg-6 d-flex flex-column justify-content-center pt-4 pt-lg-0 order-2 order-lg-1" data-aos="fade-up" data-aos-delay="200">
-                        <h1>Learn the secrets of the Web Development</h1>
-                        <h2>We are team of talented designers ready to make your learning easier</h2>
-                        <div className="d-lg-flex">
-                            <Link to="/Registration" className="btn-get-started scrollto" >Get Started</Link>
+                    <div className="row">
+                        <div className="col-lg-6 d-flex flex-column justify-content-center ml-5 pt-lg-0 order-2 order-lg-1" data-aos="fade-up" data-aos-delay="200">
+                            <h1>Learn the secrets of the Web Development</h1>
+                            <p>We are team of talented designers ready to make your learning easier</p>
+                            <div className="d-lg-flex">
+                                <Link to="/Registration" className="btn-grad" >Get Started</Link>
+                            </div>
+                        </div>
+                        <div className="col-lg-6 order-1 order-lg-2 hero-img" data-aos="zoom-in" data-aos-delay="200">
+                            <img src="./images/hero-img.png" className="img-fluid animated" alt=""/>
                         </div>
                     </div>
-                    <div className="col-lg-6 order-1 order-lg-2 hero-img" data-aos="zoom-in" data-aos-delay="200">
-                         <img src="./images/hero-img.png" className="img-fluid animated" alt=""/>
-                    </div>
                 </div>
-                </div>
-             </section>
-
-             
-
-            </div>
-        )
-    }
+             </section>           
+)}
 }
 
 export default Home
--- src/Photo.js
+++ src/Photo.js
@@ -8,22 +8,22 @@ export class Photo extends Component {
                        <h2>Now you can code everywhere. Don't hesitate to travel!</h2>
                        <br></br>
                        <div className="photo-galery">
-                        <img src="./images/travel.jpg" className="rounded" width="350px" alt=""></img>
-                        <img src="./images/travel2.jpg" className="rounded" width="350px"  alt=""></img>
-                        <img src="./images/travel3.jpg" className="rounded" width="350px"  alt=""></img>
+                        <img src="./images/travel.jpg" className="rounded" alt=""></img>
+                        <img src="./images/travel2.jpg" className="rounded"  alt=""></img>
+                        <img src="./images/travel3.jpg" className="rounded"  alt=""></img>
                         </div>
                         
                 <h2> A few of our offices:</h2>
                 <div className="photo-galery">
-                        <img src="./images/office1.jpg" className="rounded" width="350px"  alt=""></img>
-                        <img src="./images/office2.jpg" className="rounded" width="350px"  alt=""></img>
-                        <img src="./images/office3.jpg" className="rounded" width="350px"  alt=""></img>
+                        <img src="./images/office1.jpg" className="rounded"  alt=""></img>
+                        <img src="./images/office2.jpg" className="rounded"  alt=""></img>
+                        <img src="./images/office3.jpg" className="rounded"  alt=""></img>
                         </div>
                 <h2> Our online and offline classes:</h2>
                 <div className="photo-galery">
-                        <img src="./images/class1.jpg" className="rounded" width="350px"  alt=""></img>
-                        <img src="./images/class2.jpg" className="rounded" width="350px"  alt=""></img>
-                        <img src="./images/class3.jpg" className="rounded" width="350px"  alt=""></img>
+                        <img src="./images/class1.jpg" className="rounded"  alt=""></img>
+                        <img src="./images/class2.jpg" className="rounded"  alt=""></img>
+                        <img src="./images/class3.jpg" className="rounded"  alt=""></img>
                         </div>
                  </div>
 
