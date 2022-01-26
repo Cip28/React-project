5f28f94dee3edaebdd28b8f895156e7bfaebf1e0
Cip28 <ciprianbarbu38@yahoo.com>
2022-01-26 12:11:30 +0200

    Final rebuilding commit..

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,22 +1,29 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
-    return (
-          <footer class="footer">
-            <div class="all" id="search-bar">
-              <div class="input-group">
-                <input type="search" class="form-control rounded" placeholder="Search" aria-label="Search"
-                  aria-describedby="search-addon" />
-                <button type="button" class="btn btn-outline-primary">search</button>
-              </div>
-            </div>
-            <h4>Designed by Barbu Ciprian</h4>
-  
-            <div class="small text-center text-muted fst-italic">Copyright &copy; Your Website 2021</div>
- 
-          </footer>
-        
-    )
+  return (
+    <footer className="footer">
+      <div className="all" id="search-bar">
+        <div className="input-group">
+          <input
+            type="search"
+            className="form-control rounded"
+            placeholder="Search"
+            aria-label="Search"
+            aria-describedby="search-addon"
+          />
+          <button type="button" className="btn btn-outline-primary">
+            search
+          </button>
+        </div>
+      </div>
+      <h4>Designed by Barbu Ciprian</h4>
+
+      <div className="small text-center text-muted fst-italic">
+        Copyright &copy; Your Website 2021
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,38 +1,67 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-    return (
-     
- <nav>      
+  return (
+    <nav>
       <ul className="links">
-        <li><Link to="/">Home</Link></li>
-        <li><Link className="desktop-link" to="">Product {'>'}</Link>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link className="desktop-link" to="#">
+            Product {">"}
+          </Link>
           <ul>
-            <li><a href="https://www.javascript.com/">Javascript</a></li>
-            <li><a href=" ">HTML/CSS</a></li>
             <li>
-              <a href=" " className="desktop-link">Frameworks {'>'}
-                <span className="fa fa-plus"><i className="fa fa-arrow-right" aria-hidden="true"></i></span>
-              </a>
-      
+              <a href="https://www.javascript.com/">Javascript</a>
+            </li>
+            <li>
+              <a href=" ">HTML/CSS</a>
+            </li>
+            <li>
+              <Link to className="desktop-link">
+                Frameworks {">"}
+                <span className="fa fa-plus">
+                  <i className="fa fa-arrow-right" aria-hidden="true"></i>
+                </span>
+              </Link>
+
               <ul>
-                <li><a href="https://reactjs.org/">React</a></li>
-                <li><a href="https://angularjs.org/">Angular</a></li>
-                <li><a href="https://vuejs.org/">Vue</a></li>
+                <li>
+                  <a href="https://reactjs.org/">React</a>
+                </li>
+                <li>
+                  <a href="https://angularjs.org/">Angular</a>
+                </li>
+                <li>
+                  <a href="https://vuejs.org/">Vue</a>
+                </li>
               </ul>
             </li>
           </ul>
         </li>
-        <li><Link to="/Registration">Registration</Link></li>
-        <li> <Link to="/Photo">Photo Galery</Link></li>
-        <li> <Link to="/Blog">Blog</Link></li>
-        <li> <Link to="/Help">Help</Link></li>
-        <li><Link to="/Contact">Contact</Link></li>
+        <li>
+          <Link to="/Registration">Registration</Link>
+        </li>
+        <li>
+          {" "}
+          <Link to="/Photo">Photo Galery</Link>
+        </li>
+        <li>
+          {" "}
+          <Link to="/Blog">Blog</Link>
+        </li>
+        <li>
+          {" "}
+          <Link to="/Help">Help</Link>
+        </li>
+        <li>
+          <Link to="/Contact">Contact</Link>
+        </li>
       </ul>
-
-  
-        </nav>
-)};
+    </nav>
+  );
+};
 
 export default Navbar;
